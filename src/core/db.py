5324b2780136d07f4484fbd13491359f8195e041
PrimadonnaGit <jwhyun2215@gmail.com>
2024-07-20 21:57:27 +0900

    fix: 오타 수정 및 테스트 코드 제거

--- src/core/db.py
+++ src/core/db.py
@@ -2,7 +2,6 @@ from sqlmodel import Session, create_engine
 
 from src.core.config import settings
 from src.models.emotions import Emotion
-from src.models.test import Test
 from src.models.users import User, Diary
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
@@ -10,6 +9,5 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 def init_db(session: Session) -> None:
     User.metadata.create_all(engine)
-    Test.metadata.create_all(engine)
     Diary.metadata.create_all(engine)
     Emotion.metadata.create_all(engine)
--- src/models/diary.py
+++ src/models/diary.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-
-from sqlmodel import Field,SQLModel
 from typing import Optional
+
+from sqlmodel import Field, SQLModel
 
 from src.utils.utils import utc_now
 
@@ -9,5 +9,5 @@ from src.utils.utils import utc_now
 class Diary(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     content: str = Field(...)
-    choosen_emotion: Optional[int] = Field(...)
+    chosen_emotion: Optional[int] = Field(...)
     created_date: datetime = Field(default_factory=utc_now)
